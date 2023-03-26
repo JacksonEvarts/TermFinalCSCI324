09f4194b875845f840b02e3adcdc09b514781cac
Jackson Evarts <jack.evarts@gmail.com>
2023-03-26 16:54:58 -0400

    Initial Commit

---
+++ TermFinal/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TermFinal
+//
+//  Created by Jack Evarts on 3/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TermFinal/TermFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TermFinalApp.swift
+//  TermFinal
+//
+//  Created by Jack Evarts on 3/26/23.
+//
+
+import SwiftUI
+
+@main
+struct TermFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
