572f61160a625a5d89ef2bc4815c3c7c4ad6139f
tccreigh <126419765+tccreigh@users.noreply.github.com>
2023-04-02 22:59:15 -0400

    FixedPoints
    Added fixed points to show some pins on the map

--- TermFinal/ContentView.swift
+++ TermFinal/ContentView.swift
@@ -7,14 +7,30 @@
 import MapKit
 import SwiftUI
 
+struct MapLocation: Identifiable {
+    let id = UUID()
+    let name: String
+    let latitude: Double
+    let longitude: Double
+    var coordinate: CLLocationCoordinate2D{
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
 
 struct ContentView: View {
-    @StateObject private var viewModel = ContentViewModel() // Making content view model observable
     
+    @StateObject private var viewModel = ContentViewModel() // Making content view model observable
+    @State var MapLocations = [MapLocation(name: "PIN 1", latitude: 42.2392, longitude: -71.8080), MapLocation(name: "PIN 2", latitude: 40.8559, longitude: -73.2007)]
     
     var body: some View {
         VStack {
-            Map(coordinateRegion: $viewModel.region, showsUserLocation: true) // shows location if we have user's permission
+            Map(coordinateRegion: $viewModel.region,
+                interactionModes: MapInteractionModes.all, showsUserLocation: true,
+                annotationItems: MapLocations,
+                annotationContent: {
+                location in MapMarker(coordinate: location.coordinate, tint: .red)
+            }
+                ) // shows location if we have user's permission
                 .ignoresSafeArea()
                 .accentColor(Color(.systemMint))
                 .onAppear {
